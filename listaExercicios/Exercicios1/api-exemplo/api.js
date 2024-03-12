1c0836884f63ed6e6c20cb6788bb30b3722ce43e
guilhermev1s <guivs56@gmail.com>
2024-03-11 21:49:20 -0300

    pasta-api

---
+++ listaExercicios/Exercicios1/api-exemplo/api.js
@@ -0,0 +1,11 @@
+const server = require('http')
+
+let contador = 0
+
+server.createServer((req, res) => {
+
+contador++
+console.log('contador de requisições: ' + contador)
+ res.write('Bem vindo ao meu backend! Contador de requisições: ' + contador)
+  res.end()
+}).listen(3000)
