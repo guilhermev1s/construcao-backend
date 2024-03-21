46f95ef80f82b11aac162fa6d590f3af7b7f60cc
guilhermev1s <guivs56@gmail.com>
2024-03-21 19:45:30 -0300

    new

--- 01-api-inicial/index.js
+++ 01-api-inicial/index.js
@@ -77,7 +77,7 @@ app.get('/exercicio1/:nota1/:nota2/:nota3/:nota4', (req, res) => {
 })
 
 // 1. Faça um Programa que receba quatro notas de um aluno, calcule e imprima a média aritmética das notas e a mensagem de aprovado para média superior ou igual a 7.0 ou a mensagem de reprovado para média inferior a 7.0.
-// Path Param
+// query Param
 app.get('/estudante', (req, res) => {
   console.log(req.query)
   const nota1 = Number((req.query.nota1))
@@ -90,9 +90,9 @@ app.get('/estudante', (req, res) => {
   const resultado = media >= 7 ? "Aprovado" : "Reprovado"
 
   res.send(`
-      Media: ${media.toFixed(1)}
-      Resultado: ${resultado}
-  `)
+  Media: ${media.toFixed(1)}
+  Resultado: ${resultado}
+`)
 })
 
 
