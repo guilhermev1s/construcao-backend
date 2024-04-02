552df461bf2184c446f4e4c730546523691c1369
guilhermev1s <guivs56@gmail.com>
2024-04-01 21:40:38 -0300

    new

--- listaExercicios/Exercicio2/index.js
+++ listaExercicios/Exercicio2/index.js
@@ -33,6 +33,43 @@ app.get('/exercicio1', (req, res) => {
     res.send(`Estoque Médio: ${estoqueMedio}`)
 })
 
+//7. Faça uma api para ler o código e o preço de 15 produtos, calcular e escrever:
+//• O maior preço lido; e
+//• A média aritmética dos preços dos produtos.
+app.post('/exercicio7', (req, res) => {
+  console.log(req.body)
+  const corpo = req.body
+  let listaProdutos = []
+
+  corpo.forEach(produto => {
+    listaProdutos.push(produto)
+  });
+
+  let soma = 0
+  listaProdutos.forEach(produto => {
+    soma = soma + produto.preco
+  })
+  
+  const media = soma / listaProdutos.length
+
+  let maiorPreco = 0
+  listaProdutos.forEach(produto => {
+    if (produto.preco > maiorPreco){
+      maiorPreco = produto.preco
+    }
+  })
+
+  const resultado = {
+    precoMedio: media.toFixed(2),
+    maiorPreco: maiorPreco
+  }
+  res.json(resultado)
+})
+
+
+
+
+
 app.listen(3000, () => {
     console.log(`Api iniciada! rodando em http://localhost:3000`)
 })
